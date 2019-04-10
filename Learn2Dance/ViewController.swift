9d22b00e407169cb40727600376148cf2859c520
William Hutt <whutt1994@gmail.com>
2019-04-10 14:12:00 -0700

    Initial Commit

---
+++ Learn2Dance/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Learn2Dance
+//
+//  Created by William Hutt on 4/10/19.
+//  Copyright © 2019 William Hutt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
