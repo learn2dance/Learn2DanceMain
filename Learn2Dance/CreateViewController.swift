f67f693bd2d4403167a83b30651a38203f016b10
William Hutt <whutt1994@gmail.com>
2019-04-15 13:16:21 -0700

    skeleton revision on older Xcode

---
+++ Learn2Dance/CreateViewController.swift
@@ -0,0 +1,35 @@
+//
+//  CreateViewController.swift
+//  Learn2Dance
+//
+//  Created by William Hutt on 4/10/19.
+//  Copyright © 2019 William Hutt. All rights reserved.
+//
+
+import UIKit
+
+class CreateViewController: UIViewController {
+
+    @IBOutlet weak var linkText: UITextField!
+    @IBOutlet weak var descriptionText: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func onUpload(_ sender: Any) {
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
